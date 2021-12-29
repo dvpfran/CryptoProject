132f521bc6c5d7d970dbe48788ce1b9c247b147b
Luís Francisco <2211911@my.ipleiria.pt>
2021-12-29 23:06:55 +0000

    Add função para guardar lista no localStorage

--- js/favorites.js
+++ js/favorites.js
@@ -1,5 +1,5 @@
 // Código para moedas favoritas.
-
+const FAVORITE_COINS = "FAVORITE_COINS";
 let favoriteCoins = [];
 
 /**
@@ -10,6 +10,7 @@ function addToFavorites(coinId) {
     // Só adiciona a moeda à lista caso ela não exista.
     if (!favoriteCoins.includes(coinId)) {
         favoriteCoins.push(coinId);
+        saveToLocalStorage();
     }
 }
 
@@ -22,5 +23,14 @@ function removeFromFavorites(coinId) {
     if (favoriteCoins.includes(coinId)) {
         let indexCoin = favoriteCoins.indexOf(coinId);
         favoriteCoins.splice(indexCoin, 1);
+        saveToLocalStorage();
     }
+}
+
+/**
+ * Vai converter a lista para uma string e depois guarda no local storage.
+ */
+function saveToLocalStorage() {
+    let stringFavoriteCoins = JSON.stringify(favoriteCoins);
+    localStorage.setItem(FAVORITE_COINS, stringFavoriteCoins);
 }
