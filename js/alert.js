ce062c562ee99fdea1e2e47ef0f06776f84b9ee4
Luís Francisco <2211911@my.ipleiria.pt>
2021-12-31 16:00:05 +0000

    Add função para adicionar alertas e guardar no localstorage

--- js/alert.js
+++ js/alert.js
@@ -25,3 +25,25 @@ class Alert {
  * Lista com todos os alertas configurados.
  */
 let alerts = [];
+
+/**
+ * Adicionar um alerta à lista.
+ * @param {number} id Id do alerta.
+ * @param {string} coinId Id da moeda configurada para o alerta.
+ * @param {number} minValue Valor mínimo da moeda para dispultar o alerta.
+ * @param {number} maxValue Valor máximo da moeda para dispultar o alerta.
+ * @param {bool} isDispatch Propriedade para verificar se o alerta já foi dispultado alguma vez.
+ * @param {bool} isSeen  Propriedade para verificar se o alerta já foi visto pelo utilizador.
+*/
+function addAlert(id, coinId, minValue, maxValue) {
+    alerts.push(new Alert(id, coinId, minValue, maxValue, false, false));
+    saveAlertsToLocalStorage();
+}
+
+/** 
+ * Vai guardar a lista das configurações dos alertas no localStorage.
+*/
+function saveAlertsToLocalStorage() {
+    let stringAlerts = JSON.stringify(alerts);
+    localStorage.setItem(ALERTS, stringAlerts);
+}
