0536596dac307bd51b7e4d2a0a188673bf6356b7
Luís Francisco <2211911@my.ipleiria.pt>
2022-01-08 15:33:54 +0000

    Add javascript code of searchBar

---
+++ js/searchBar.js
@@ -0,0 +1,44 @@
+// Código para gerir o resutaldo de pesquisas.
+let allCoins = [];
+
+/**
+ * Vai devolver uma lista conforme o que é escrito na caixa de texto.
+ * @param {event} event Propriedades da caixa de texto.
+ */
+async function getSearchResults(event) {
+    const filter = this.event.target.value.toLowerCase();
+    let coinsResult = [];
+
+    // Se tiver vazio não vai fazer nada.
+    if (filter != "") {
+        // Verifica se existe alguma moeda com o filtro.
+        // Vai passar o filtro com todas as letras minúsculas.
+        if (isFilterValid(filter)) {
+            coinsResult = getFilteredCoins(filter);
+        }
+    }
+}
+
+/**
+ * Função para confirmar se o filtro existe na lista.
+ * @param {string} filter Filtro da moeda a ser pesquisada.
+ * @return {bool} Vai retornar verdadeiro se algum valor for encontrado.
+ */
+function isFilterValid(filter) {
+    return allCoins.some(x =>
+        x.id.toLowerCase().startsWith(filter) ||
+        x.name.toLowerCase().startsWith(filter) ||
+        x.symbol.toLowerCase().startsWith(filter));
+}
+
+/**
+ * Função para retornar todas moedas com o filtro pasado por parâmetro.
+ * @param {string} filter Filtro da moeda a ser pesquisada.
+ * @return {JSON[]} Vai retornar uma lista JSON com todas as moedas encontradas.
+ */
+function getFilteredCoins(filter) {
+    return allCoins.filter(x =>
+        x.id.toLowerCase().startsWith(filter) ||
+        x.name.toLowerCase().startsWith(filter) ||
+        x.symbol.toLowerCase().startsWith(filter));
+}
