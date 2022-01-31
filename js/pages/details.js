0ff390c5c965ed0617f333eb2d329946429762bd
Luís Francisco <2211911@my.ipleiria.pt>
2022-01-31 23:53:20 +0000

    Add aviso quando a moeda antige um certto valor

--- js/pages/details.js
+++ js/pages/details.js
@@ -23,6 +23,21 @@ window.onload = async () => {
         document.getElementById("warning-details-coin").style.visibility = "visible";
     }
     handlersBarraPesquisa();
+    
+    if (alerts.length > 0) {
+        for(let index = 0; index < alerts.length; index++) {
+            const coin = await getCoin(alerts[index].coinId);
+            if (coin != undefined) {
+                console.log(alerts[index].minValue);
+                console.log(alerts[index].maxValue);
+                console.log(coin.market_data.current_price.usd);
+                if (parseFloat(coin.market_data.current_price.usd) >= alerts[index].minValue && parseFloat(coin.market_data.current_price.usd) <= alerts[index].maxValue) {
+                    const toast = createToast("", "success", "Moeda antigiu certo valor.", `A moeda ${coin.name} antigiu o valor entre ${alerts[index].minValue} e ${alerts[index].maxValue}`);
+                    document.getElementById("content-toast").append(toast);
+                }
+            }
+        }
+    }
 }
 
 function mostrarMoeda(moeda) {
@@ -197,10 +212,10 @@ function addAlert() {
     btnSave.addEventListener("click", () => {
         let toast;
         if (saveAlert(nextId, coinIdDetails, inputMin.value, inputMax.value)) {
-            toast = createToast("", "success", "Alerta guardado", "O alerta foi guardado com sucesso")
+            toast = createToast("", "success", "Alerta guardado", "O alerta foi guardado com sucesso");
         }
         else {
-            toast = createToast("", "error", "Erro ao guardar alerta", "Os valores mínimo não pode ser que o valor máximo ou ambos os valores não podem ser 0.")
+            toast = createToast("", "error", "Erro ao guardar alerta", "Os valores mínimo não pode ser que o valor máximo ou ambos os valores não podem ser 0.");
         }
         document.getElementById("content-toast").append(toast);
     });
--- js/pages/index.js
+++ js/pages/index.js
@@ -13,6 +13,23 @@ window.onload = async () => {
     allCoins = await getAllCoins();
     handlersBarraPesquisa();
     document.getElementById("barraPesquisa").focus();
+    
+    loadAlertsFromLocalStorage();
+
+    if (alerts.length > 0) {
+        for(let index = 0; index < alerts.length; index++) {
+            const coin = await getCoin(alerts[index].coinId);
+            if (coin != undefined) {
+                console.log(alerts[index].minValue);
+                console.log(alerts[index].maxValue);
+                console.log(coin.market_data.current_price.usd);
+                if (parseFloat(coin.market_data.current_price.usd) >= alerts[index].minValue && parseFloat(coin.market_data.current_price.usd) <= alerts[index].maxValue) {
+                    const toast = createToast("", "success", "Moeda antigiu certo valor.", `A moeda ${coin.name} antigiu o valor entre ${alerts[index].minValue} e ${alerts[index].maxValue}`);
+                    document.getElementById("content-toast").append(toast);
+                }
+            }
+        }
+    }
 }
 
 function numberFormat(locale, currency, value) {
